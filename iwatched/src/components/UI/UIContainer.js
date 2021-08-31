6c6a02f9db464bb451d8f3b7eb749701ecb61017
Rodrigo Cury <rodrigocury@ufu.br>
2021-08-30 23:31:46 -0300

    UIContainer

--- iwatched/src/components/UI/UIContainer.js
+++ iwatched/src/components/UI/UIContainer.js
@@ -3,9 +3,6 @@ import styled from 'styled-components'
 const UIContainer = styled.div`
     max-width: 1000px;
     margin: 15px auto;
-    hover {
-        background-color: rgba(0, 0, 0);
-    }
 `
 
 export default UIContainer
