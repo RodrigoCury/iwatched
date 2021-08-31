0756a19baf92687b58df8eade80f974a6e15af4c
Rodrigo Cury <rodrigocury@ufu.br>
2021-08-30 23:33:01 -0300

    Added The cards and useApi Hook

--- iwatched/src/pages/Search/Search.js
+++ iwatched/src/pages/Search/Search.js
@@ -1,13 +1,40 @@
-import React from 'react'
-import UIContainer from '../../components/UI/UIContainer'
-import {Header, Title} from '../../components/UI/UIHeader'
+import Card from 'components/Card/Card'
+import React, {useEffect} from 'react'
+import UIContainer from 'components/UI/UIContainer'
+import {Header, Title} from 'components/UI/UIHeader'
+import { UIBody } from 'components/UI/UIBody'
+import useApi from 'components/utils/useApi'
 
-const Container = ({ children }) => {
+const Container = () => {
+
+    const [fetchMovies, movies] = useApi({
+        url: "http://localhost:3340/movies",
+        method: "GET",
+        params: {
+            _sort: "id",
+            _order: "desc"
+        }
+    })
+
+    useEffect(() => {
+        fetchMovies({})
+    }, [])
+
     return (
         <UIContainer>
             <Header>
                 <Title to='/'>iWatched</Title>
             </Header>
+            <UIBody>
+                {movies.data && 
+                !movies.loading &&
+                    movies.data.map(movie => (
+                        <Card movie={movie} key={movie.id}/>
+                    ))
+                }
+                {movies.loading && <h1>Carregando...</h1>}
+                {movies.error && <h3>Houve um erro ao Contatar o Servidor</h3>}
+            </UIBody>
         </UIContainer>
     )
 }
