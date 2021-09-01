765b476810092df98915f401e12fa530cf4a12d6
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-01 15:02:57 -0300

    New Search page with actual Search

--- iwatched/src/pages/Search/Search.js
+++ iwatched/src/pages/Search/Search.js
@@ -1,35 +1,40 @@
-import Card from 'components/Card/Card'
-import React, {useEffect} from 'react'
-import UIBody from 'components/UI/UIBody'
-import useApi from 'components/utils/useApi'
+import Card from "components/Card/Card";
+import React, { useState } from "react";
+import UIBody, { Body } from "components/UI/UIBody";
+import useApi from "components/utils/useApi";
+import UIInput from "components/UI/UIInput";
 
 const Container = () => {
+  const [search, setSearch] = useState("");
+  
+  const [fetchMovies, movies] = useApi({
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    params: {
+      api_key: '4d751774b43dab4d91f60e7ee588cf1f',
+      query: search
+    },
+  });
 
-    const [fetchMovies, movies] = useApi({
-        url: "http://localhost:3340/movies",
-        method: "GET",
-        params: {
-            _sort: "id",
-            _order: "desc"
-        }
-    })
+  return (
+    <UIBody>
+      <UIInput
+        name="movie-input"
+        value={search}
+        onChange={(ev) => setSearch(ev.target.value)}
+        onKeyPress={(ev) => ev.key === "Enter" && fetchMovies()}
+        placeholder="Pesquisar..."
+        large
+      ></UIInput>
+      <Body>
+        {movies.data &&
+          !movies.loading &&
+          movies.data.results.map((movie) => <Card movie={movie} key={movie.id} />)}
+        {movies.loading && <h1>Carregando...</h1>}
+        {movies.error && <h3>Houve um erro ao Contatar o Servidor</h3>}
+      </Body>
+    </UIBody>
+  );
+};
 
-    useEffect(() => {
-        fetchMovies({})
-    }, [])
-
-    return (        
-        <UIBody>
-            {movies.data && 
-            !movies.loading &&
-                movies.data.map(movie => (
-                    <Card movie={movie} key={movie.id}/>
-                ))
-            }
-            {movies.loading && <h1>Carregando...</h1>}
-            {movies.error && <h3>Houve um erro ao Contatar o Servidor</h3>}
-        </UIBody>
-    )
-}
-
-export default Container
+export default Container;
