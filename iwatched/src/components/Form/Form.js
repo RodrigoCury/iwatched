afac04d1a4bd9bea0b6b31d81d8067d6209e93f5
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-02 16:05:17 -0300

    RM

--- iwatched/src/components/Form/Form.js
+++ iwatched/src/components/Form/Form.js
@@ -2,7 +2,6 @@ import React from "react";
 import UIButton from "components/UI/UIButton";
 import UIInput from "components/UI/UIInput";
 import styled from "styled-components";
-import { Body } from "components/UI/UIBody";
 import PropTypes from "prop-types";
 
 export const Form = styled.form`
@@ -24,13 +23,11 @@ export default function UIForm({ fields, title, onSubmit, buttons }) {
       {fields.map((tag) => (
         <UIInput {...tag} key={tag.id} />
       ))}
-      <Body>
-        {buttons.map((tag, idx) => (
-          <UIButton {...tag} key={idx}>
-            {tag.innerText}
-          </UIButton>
+      {buttons.map((tag, idx) => (
+        <UIButton {...tag} key={idx}>
+          {tag.innerText}
+        </UIButton>
         ))}
-      </Body>
     </Form>
   );
 }
