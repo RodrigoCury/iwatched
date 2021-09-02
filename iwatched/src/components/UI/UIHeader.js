b10b753ae63a6a763a5a2fbf5e3b992f909c5e35
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-02 16:14:27 -0300

    slim Header

--- iwatched/src/components/UI/UIHeader.js
+++ iwatched/src/components/UI/UIHeader.js
@@ -1,39 +1,37 @@
-import React, {useContext} from "react"
-import StoreContext from "components/Store/Context"
-import styled from "styled-components"
-import {Link} from "react-router-dom"
-import UIUserHeader from "./UIUserHeader"
-
+import React, { useContext } from "react";
+import StoreContext from "components/Store/Context";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import UIUserHeader from "./UIUserHeader";
 
 const Title = styled(Link)`
-    color: #FFFFFF;
-    font-size: 30px;
-    text-decoration: none;
-    font-weight: bold;
-    align-self: center;
-`
+  color: #ffffff;
+  font-size: 30px;
+  text-decoration: none;
+  font-weight: bold;
+  align-self: center;
+`;
 
 const Header = styled.header`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    margin: 25px auto;
-    max-width: 1000px;
-    `
-    
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px auto 10px auto;
+  max-width: 1000px;
+`;
+
 export default function UIHeader() {
-    function logOut() {
-        setToken(null)
-    }
-        
-    const {token,setToken} = useContext(StoreContext)
+  function logOut() {
+    setToken(null);
+  }
 
+  const { token, setToken } = useContext(StoreContext);
 
-    return (
-        <Header>
-            <Title to='/'>iWatched</Title>
-            <UIUserHeader token={token} logOut={logOut}/>
-        </Header>
-    )
+  return (
+    <Header>
+      <Title to="/">iWatched</Title>
+      <UIUserHeader token={token} logOut={logOut} />
+    </Header>
+  );
 }
