42f96a4f3dfae42fa55b528aa952377867743c56
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-02 16:07:15 -0300

    Base Pages Component

---
+++ iwatched/src/components/UI/UIPages/Pages.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import useApi from "components/utils/useApi";
+import UIInput from "components/UI/UIInput";
+import PropTypes from 'prop-types'
+import UIHeader from "components/UI/UIHeader";
+import { Container, CardContainer } from "../UIContainer/UIContainer";
+
+
+export const BasePageNoSearch = ({children}) => (
+  <>
+    <UIHeader/>
+    <Container>
+        {children}
+    </Container>
+  </>
+);
+
+
+export const BasePage = ({children}) => {
+  const [search, setSearch] = useState("");
+
+  const [query, setQuery] = useState("");
+  
+  const [fetchMovies, movies] = useApi({
+    url: "https://api.themoviedb.org/3/search/movie",
+    method: "GET",
+    params: {
+      api_key: '4d751774b43dab4d91f60e7ee588cf1f',
+      query: search
+    },
+  });
+
+  function Search(ev) {
+    if (ev.key === "Enter" && search !== ""){
+      setQuery(search)
+      fetchMovies()
+    }
+  }
+
+  return (
+    <>
+      <UIHeader/>
+      <UIInput
+        name="movie-input"
+        value={search}
+        onChange={(ev) => setSearch(ev.target.value)}
+        onKeyPress={Search}
+        placeholder="Pesquisar..."
+        large
+      />
+      {
+        search.length && (movies.data || movies.loading || movies.error) 
+        ? (
+            <CardContainer search data={movies} query={query ?? null} />
+        ): children
+      }
+  </>
+  );
+};
+
+BasePage.propTypes = {
+  children: PropTypes.any,
+}
+
+BasePageNoSearch.propTypes = {
+  children: PropTypes.any,
+}
+
