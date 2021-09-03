c1c8e485fef1d3dd4c5cb7a9f84171c414000d07
Rodrigo Cury <rodrigocury@ufu.br>
2021-09-03 12:39:32 -0300

    Index Page Functional

--- iwatched/src/pages/Index/Index.js
+++ iwatched/src/pages/Index/Index.js
@@ -4,6 +4,8 @@ import React, {
 import { BasePage } from 'components/UI/UIPages/Pages'
 import StoreContext from 'components/Store/Context'
 import WelcomeDiv from 'components/WelcomeDiv/WelcomeDiv'
+import { Container } from 'components/UI/UIContainer/UIContainer'
+import Trending from 'components/UI/UIContainer/Trending'
 
 const Index = () => {
   const {token} = useContext(StoreContext)
@@ -11,6 +13,18 @@ const Index = () => {
   return (
     <BasePage>
       <WelcomeDiv token={token}/>
+      <Container trendingMovies trend>
+        <Trending url='https://api.themoviedb.org/3/movie/top_rated' title='Filmes Melhor Avaliados' topRated/>
+        <Trending url='https://api.themoviedb.org/3/movie/popular'title='Filmes Favoritos da Comunidade' popular/>
+        <Trending url='https://api.themoviedb.org/3/movie/upcoming' title='Próximos Lançamentos' upcoming/>
+        <Trending url='https://api.themoviedb.org/3/movie/now_playing'title='Nos Cinemas' inTheater/>
+      </Container>
+      <Container trendingSeries trend>
+        <Trending url='https://api.themoviedb.org/3/tv/top_rated' title='Séries Melhor Avaliadas' topRated/>
+        <Trending url='https://api.themoviedb.org/3/tv/popular'title='Séries Favoritas da Comunidade' popular/>
+        <Trending url='https://api.themoviedb.org/3/tv/on_the_air' title='Séries No Ar' upcoming/>
+        <Trending url='https://api.themoviedb.org/3/tv/airing_today'title='Séries No Ar Hoje' inTheater/>
+      </Container>
     </BasePage>
   )
 }
